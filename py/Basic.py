1eb9d2ee15008884fe97586761aaa3ee1f0eaebf
DingChil <xu.dingchao@gmail.com>
2021-02-03 23:51:55 +0800

    feat(Basic.py): add variable demo.

--- py/Basic.py
+++ py/Basic.py
@@ -8,3 +8,11 @@ print('^: ' + str(pre^num))
 print('~: ' + str(~num))
 print('<<: ' + str(pre<<num))
 print('>>: ' + str(pre>>num))
+tmp = 123
+print(tmp)
+tmp = '321'
+print(tmp)
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
